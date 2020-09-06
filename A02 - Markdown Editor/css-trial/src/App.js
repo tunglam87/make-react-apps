099072c4599b3fe3bf433ed7729da305c8d01e3f
TungLam@Siushan Mac <tunglam0915@yahoo.com.hk>
2020-09-07 01:39:50 +0800

    css tutorial 3

---
+++ A02 - Markdown Editor/css-trial/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <textarea />
+      <div className="preview"></div>
+    </div>
+  );
+}
+
+export default App;
